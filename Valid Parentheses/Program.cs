bdfd3db08c279a55975fa8211129c00aa679d41e
Jakub Kostrzewa <kubacfaniak@gmail.com>
2023-09-28 19:31:39 +0200

    Valid Parentheses solved

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> myStack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                myStack.Push(')');
+            }
+            else if (c == '[')
+            {
+                myStack.Push(']');
+            }
+            else if (c == '{')
+            {
+                myStack.Push('}');
+            }
+            else if (myStack.Count == 0 || myStack.Pop() != c)
+            {
+                return false;
+            }
+        }
+        return myStack.Count == 0;
+    }
+}
