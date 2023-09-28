240baed70b1daa18abf61dd8fcd4e9983f310662
Jakub Kostrzewa <kubacfaniak@gmail.com>
2023-09-28 19:28:57 +0200

    Roman to Integer solved

--- Palindrome Numbers/Program.cs
+++ Palindrome Numbers/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        string str = x.ToString();
+        int left = 0;
+        int right = str.Length - 1;
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
