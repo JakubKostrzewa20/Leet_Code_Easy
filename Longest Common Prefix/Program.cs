775c8d77d37adcf145fc06b25948c24bb39b44d1
Jakub Kostrzewa <kubacfaniak@gmail.com>
2023-09-28 19:30:20 +0200

    Longest Common Prefix solved

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int minLength = strs.Min(x => x.Length);
+        string shortest_element = strs.FirstOrDefault(x => x.Length == minLength);
+        foreach (string item in strs)
+        {
+            for (int j = 0; j < minLength; j++)
+            {
+                if (item[j] != shortest_element[j])
+                {
+                    minLength = j;
+                    break;
+                    if (minLength == 0)
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+        return shortest_element.Substring(0, minLength);
+    }
+}
--- Roman to Integer/Program.cs
+++ Roman to Integer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        var Roman = new Dictionary<char, int>();
+        Roman.Add('I', 1);
+        Roman.Add('V', 5);
+        Roman.Add('X', 10);
+        Roman.Add('L', 50);
+        Roman.Add('C', 100);
+        Roman.Add('D', 500);
+        Roman.Add('M', 1000);
+        int last = 0;
+        int sum = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int current = Roman[s[i]];
+            if (current < last)
+            {
+                sum -= current;
+            }
+            else
+            {
+                sum += current;
+            }
+            last = current;
+        }
+        return sum;
+    }
+}
